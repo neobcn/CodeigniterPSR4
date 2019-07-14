bf20d3614bc80c4de76d5b9c03d325f464e809fe
Salvador Mata <smap.develop@gmail.com>
2019-07-14 16:29:31 +0200

    Refactoring

---
+++ tests/HelloWorld/SayHello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: neobcn
+ * Date: 14/07/2019
+ * Time: 13:44
+ */
+
+namespace HelloWorld;
+
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Hello World, Composer!';
+    }
+}
